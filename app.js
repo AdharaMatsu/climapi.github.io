0929ed3479846e80fcbd04ec68b292ecb04e3c00
Adhara Cavazos <95373792+AdharaMatsu@users.noreply.github.com>
2023-04-19 15:27:38 -0600

    agregando s en la ruta de la api

--- app.js
+++ app.js
@@ -18,34 +18,34 @@ function callAPI() {
     console.log(latLot);
     switch(data.archivo) {
         case '1':
-            request = `http://api.weatherapi.com/v1/current.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/current.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '2':
-            request = `http://api.weatherapi.com/v1/forecast.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/forecast.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '3':
-            request = `http://api.weatherapi.com/v1/search.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/search.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '4':
-            request = `http://api.weatherapi.com/v1/history.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/history.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '5':
-            request = `http://api.weatherapi.com/v1/marine.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/marine.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '6':
-            request = `http://api.weatherapi.com/v1/future.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/future.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '7':
-            request = `http://api.weatherapi.com/v1/timezone.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/timezone.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '8':
-            request = `http://api.weatherapi.com/v1/sports.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/sports.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '9':
-            request = `http://api.weatherapi.com/v1/astronomy.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/astronomy.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
         case '10':
-            request = `http://api.weatherapi.com/v1/ip.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
+            request = `https://api.weatherapi.com/v1/ip.json?key=d655185fb8434fcebcb160306232903&q=${(data.city != '' ? data.city : latLot)}&dt=${data.date}`
             break
     }
 
